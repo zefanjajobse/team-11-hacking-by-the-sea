f3671eb195b55a6181e741072304bccc4e5a72c3
niek726 <schl0014@hz.nl>
2022-07-05 19:09:36 +0200

    upload does it

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -34,3 +34,4 @@ class FileUploadController extends Controller
 
     }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('file-upload', [FileUploadController::class, 'index'])->name('file-upload.index');
-Route::post('store', [FileUploadController::class, 'store'])->name('file-upload.store');
-Route::get('/pdf/{file}', function ($file) {
-    $path = public_path('storage\files\D4QyUTWQq9BF9vBabztsfuTiKc735a4RI7hwsDlZ.pdf');
-
-    $header = [
-        'Content-Type' => 'application/pdf',
-        'Content-Disposition' => 'inline; filename="' . $file . '"'
-    ];
-    return response()->file($path, $header);
-})->name('pdf');
+Route::get('file-upload', [FileUploadController::class, 'index']);
+Route::post('store', [FileUploadController::class, 'store']);
 
 require __DIR__.'/auth.php';
