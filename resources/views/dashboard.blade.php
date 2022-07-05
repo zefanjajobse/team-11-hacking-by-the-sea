332f9022fac2a5487df34eb15996ef31bbb1df54
Zefanja Jobse <zefanjaj@gmail.com>
2022-07-05 21:53:59 +0200

    .

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,32 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                Add
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <x-dropdown-link
+                                    onclick="event.preventDefault(); console.log('yeet');">
+                                {{ __('LBody') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link
+                                    onclick="event.preventDefault(); console.log('yeet');">
+                                {{ __('Body') }}
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
+
+
                     <form method="POST" action="{{ route('dashboard') }}">
                         @csrf
                         <!-- nanme -->
